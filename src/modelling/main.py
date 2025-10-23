9a5afdcfbeebbde7f45fe9fb664a7188e6d1e8c8
Felix Muller <felix_myu@Felixs-MacBook-Pro-362.local>
2025-10-23 15:59:47 +0200

    added data path as default value to main function. changed libraries import paths in src/modelling/main.py

--- src/modelling/main.py
+++ src/modelling/main.py
@@ -1,13 +1,13 @@
 # This module is the training flow: it reads the data, preprocesses it, trains a model and saves it.
 
 import argparse
-from .utils import load_data
-from .preprocessing import encode_sex, splitting_data
-from .training import train
+from src.modelling.utils import load_data
+from src.modelling.preprocessing import encode_sex, splitting_data
+from src.modelling.training import train
 from pathlib import Path
 
 
-def main(trainset_path: Path) -> None:
+def main(trainset_path: Path = Path("data/abalone.csv")) -> None:
     """Train a model using the data at the given path and save the model (pickle)."""
     # Read data
     print("Loading data...")
@@ -29,6 +29,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Train a model using the data at the given path."
     )
-    parser.add_argument("trainset_path", type=str, help="Path to the training set")
+    parser.add_argument(
+        "--trainset_path",
+        type=str,
+        default="data/abalone.csv",
+        help="Path to the training set (default: data/abalone.csv)",
+    )
     args = parser.parse_args()
-    main(args.trainset_path)
+    main(Path(args.trainset_path))
